ce6c96482f95a18e10e5241217ee2a9d7ddc8f71
Iury Oliveira <iury@rarolabs.com.br>
2020-04-27 23:10:44 -0300

    create transaction with custom category

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,56 @@
 // import AppError from '../errors/AppError';
 
-import Transaction from '../models/Transaction';
+import { getRepository } from 'typeorm';
 
+import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import CreateCategoryService from './CreateCategoryService';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryName: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryName,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getRepository(Transaction);
+    const categoryRepository = getRepository(Category);
+
+    let categoryId;
+
+    const findCategory = await categoryRepository.findOne({
+      where: { title: categoryName },
+    });
+
+    if (!findCategory) {
+      const createCategory = new CreateCategoryService();
+
+      const category = await createCategory.execute({ title: categoryName });
+
+      await categoryRepository.save(category);
+
+      categoryId = category.id;
+    } else {
+      categoryId = findCategory.id;
+    }
+
+    const transaction = transactionRepository.create({
+      title,
+      type,
+      value,
+      category_id: categoryId,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
