4d39be4adde51aebd8813e0f87d3cc66f0614237
Iury Oliveira <iury@rarolabs.com.br>
2020-04-29 01:44:58 -0300

    import transaction

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,13 +1,22 @@
 import { Router } from 'express';
+import { getCustomRepository, Transaction } from 'typeorm';
 
-import { getCustomRepository } from 'typeorm';
+import path from 'path';
+
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import loadCSV from '../utils/loadCSV';
 
 const transactionsRouter = Router();
+
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionRepository = getCustomRepository(TransactionsRepository);
@@ -44,8 +53,30 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.status(204).json();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('transactions'),
+  async (request, response) => {
+    const { file } = request;
+
+    const csvFilePath = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'tmp',
+      file.filename,
+    );
+
+    const transactionsData = await loadCSV(csvFilePath);
+
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute({
+      transactionsData,
+    });
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,32 @@
+import { getRepository } from 'typeorm';
+
 import Transaction from '../models/Transaction';
 
+import CreateTransactionService from './CreateTransactionService';
+
+interface Request {
+  transactionsData: Transaction[];
+}
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(transactionsData: Request): Promise<Transaction[]> {
+    const createTransaction = new CreateTransactionService();
+
+    const transactionList = transactionsData.transactionsData.map(
+      async transaction => {
+        const newTransaction = createTransaction.execute({
+          title: transaction[0],
+          type: transaction[1],
+          value: transaction[2],
+          categoryName: transaction[3],
+        });
+
+        return newTransaction;
+      },
+    );
+
+    const resultado = await Promise.all(transactionList);
+    return resultado;
   }
 }
 
